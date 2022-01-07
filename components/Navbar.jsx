c83d2c77164359ace5bce441f00a4f30df0efe69
Saeed Khan <saeed@skdevelopment.org>
2022-01-07 02:38:13 -0700

    added redux with redux toolkit

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import Image from 'next/image';
 import styles from '../styles/Navbar.module.css';
+import { useSelector } from 'react-redux';
+
 const Navbar = () => {
+
+    const quantity = useSelector(state => state.cart.quantity);
+
     return (
         <div className={styles.container}>
         <div className={styles.item}>
@@ -29,7 +34,7 @@ const Navbar = () => {
                 <div className={styles.item}>
                     <div className={styles.cart}>
                     <Image src="/assets/cart.png" alt='logo' height="30px" width="30px" />
-                 <div className={styles.counter}>2</div>
+                 <div className={styles.counter}>{quantity}</div>
         </div>  
         </div>
         </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout.js';
+import store from '../redux/store';
+import { Provider } from 'react-redux';
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Provider store={store}>
   <Layout>
     <Component {...pageProps} />
   </Layout>
+    </Provider>
   )
 }
 
